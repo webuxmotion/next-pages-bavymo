62e953484229c22f48ad258de91c73ad9515033a
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2025-08-17 16:49:21 +0300

    t

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -3,7 +3,7 @@ import styles from "./Header.module.scss";
 export default function Header() {
     return (
         <header className={styles.container}>
-            <h1 className={styles.title}>Updated!</h1>
+            <h1 className={styles.title}>Updated! !</h1>
             <p>Using SCSS variables, nesting, and modules.</p>
         </header>
     );
